a57d14b18823ac818cd180b0f06431a81c770ef6
Jason Righelato <jayontraps@gmail.com>
2016-09-22 18:57:57 +0100

    update keys markup

--- page-maps-overview.php
+++ page-maps-overview.php
@@ -55,7 +55,38 @@ get_header('maps'); ?>
                                 <?php include('inc/maps/map-grid-1.php'); ?>
                             </div>
                         </div>
-                        <?php include('inc/maps/map-key-dbreed.php'); ?>
+
+                        <table class="key-table state counts ">
+                            <tbody>
+                                <tr>
+                                    <td class="headTd"></td>
+                                    <td>present</td>
+                                    <td>possible</td>
+                                    <td>probable</td>
+                                    <td>confirmed</td>
+                                </tr>
+                                <tr data-set-key="dbreed" >
+                                    <td class="headTd">2007 - 13:</td>
+                                    <td>
+                                        <span class="keysq brst present"></span>
+                                        <span class="tet-data pres-target"></span>
+                                    </td>
+                                    <td>
+                                        <span class="keysq brst poss"></span>
+                                        <span class="tet-data poss-target"></span>
+                                    </td>
+                                    <td>
+                                        <span class="keysq brst prob"></span>
+                                        <span class="tet-data prob-target"></span>
+                                    </td>
+                                    <td>
+                                        <span class="keysq brst confirmed"></span>
+                                        <span class="tet-data conf-target"></span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
 
