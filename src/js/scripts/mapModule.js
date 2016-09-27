e5be67491d09f173bb9372e5d83e1cc2e7c009b3
Jason Righelato <jayontraps@gmail.com>
2016-09-27 19:43:59 +0100

    gmap link bug

--- src/js/scripts/mapModule.js
+++ src/js/scripts/mapModule.js
@@ -52,7 +52,7 @@ MapModule.prototype.setGoogleMapLink = function() {
     gMapWrap.empty();
 
     if (this.tetrad.active) {
-        var url = window.location.hostname;
+        var url = '//' + window.location.hostname;
         var gMapLink = $('<a/>', {
             'href': url + '/gmap/?tetrad=' + this.tetrad.active,
             'target': '_blank',
