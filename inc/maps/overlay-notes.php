ab05c4725d9d32dd012a6384e720b31d8b016978
Jason Righelato <jayontraps@gmail.com>
2016-09-28 19:11:37 +0100

    keys link

--- inc/maps/overlay-notes.php
+++ inc/maps/overlay-notes.php
@@ -2,6 +2,6 @@
 
     <div class="foot-note no-m-p small-text">Based upon LCM2007 &copy; NERC (CEH) 2011. Contains Ordnance Survey data <br> &copy; Crown Copyright 2007. &copy; third party licensors.</div>
 
-    <a href="#" target="_blank" class="hab-key-link small-text">Habitat keys</a>
+    <a href="<?php bloginfo('url'); ?>/tetrad-maps/distribution-maps/habitat-topography/" target="_blank" class="hab-key-link small-text">Habitat keys</a>
 
 </div>
